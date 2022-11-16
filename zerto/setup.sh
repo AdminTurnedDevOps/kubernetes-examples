959764fcc591b114423c3d3ac9f02825037ce311
Michael Levan <mlevan1992@gmail.com>
2022-11-16 07:19:43 -0500

    nginx

---
+++ zerto/setup.sh
@@ -0,0 +1,35 @@
+`helm repo add zerto https://zapps-helm.zerto.com/z4k/stable`
+
+`helm repo update`
+
+`kubectl create namespace zerto`
+
+## Set your global configurations
+
+```
+global:
+  imagePullSecret: "your_customer_pull_secret"
+  authentication:
+    adminUser: "zerto"
+    adminPassword: "zerto"
+    managementUser: "zerto"
+    managementPassword: "zerto"
+
+zkm-px:
+  config:
+    siteId: "your_site_id"
+```
+
+`helm install zerto zerto/z4k -f value.yaml --namespace zerto`
+
+`kubectl get all -n zerto`
+
+## Zerto CLI Config
+
+`wget https://z4k.zerto.com/kubectl-zrt`
+
+`chmod +x kubectl-zrt`
+
+`cp kubectl-zrt /usr/bin`
+
+`kubectl zrt`
