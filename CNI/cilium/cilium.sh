0f172e534c10916c9e83f0562f8e40d20ff8ef86
michael levan <michael.levan@orbica.world>
2023-04-25 07:18:48 -0400

    cilium

--- CNI/cilium/cilium.sh
+++ CNI/cilium/cilium.sh
@@ -1,14 +1,13 @@
-
 helm repo add cilium https://helm.cilium.io/
 
   helm install cilium cilium/cilium \
   --namespace kube-system \
-  --set azure.enabled=true \
-  --set eni.enabled=true \
+  --set egressMasqueradeInterfaces=eth0 \
   --set tunnel=disabled \
+  --set eni.enabled=true \
+  --set ipam.mode=eni \
   --set nodeinit.enabled=true \
-  --set ipam.mode=azure \
-  --set masquerade=false
+  --set kubeProxyReplacement=strict
 
 kubectl delete ds -n kube-system aws-node                       
 kubectl delete ds -n kube-system kube-proxy
