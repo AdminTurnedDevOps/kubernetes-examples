b57dde6ed6d064d740079fb3fc286babf34a5049
michael levan <michael.levan@orbica.world>
2023-08-17 09:21:45 -0400

    cluster api

--- cluster-api/mgmtinit-azure.sh
+++ cluster-api/mgmtinit-azure.sh
@@ -3,12 +3,12 @@ export CLUSTER_TOPOLOGY=true
 ```
 
 ```
-export AZURE_SUBSCRIPTION_ID=""
+export AZURE_SUBSCRIPTION_ID="220284d2-6a19-4781-87f8-5c564ec4fec9"
 
 # Create an Azure Service Principal and paste the output here
-export AZURE_TENANT_ID=""
-export AZURE_CLIENT_ID=""
-export AZURE_CLIENT_SECRET=""
+export AZURE_TENANT_ID="fbbd9689-2869-42b8-92fb-db1c89b662ff"
+export AZURE_CLIENT_ID="94b0aa03-e57f-46a9-a511-130271d4756d"
+export AZURE_CLIENT_SECRET="WtB8Q~EUQY5dd11rb2avYC2QyUYE7SFj6M-B.cdN"
 
 # Base64 encode the variables
 export AZURE_SUBSCRIPTION_ID_B64="$(echo -n "$AZURE_SUBSCRIPTION_ID" | base64 | tr -d '\n')"
