bebd614c4bb4e1bb5ec2e74714bd78afc54741b3
adminturneddevops <mlevan1992@gmail.com>
2023-10-02 15:10:11 -0400

    PVCs

---
+++ volumes/NFS_CSI/install.sh
@@ -0,0 +1,2 @@
+helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
+helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs --namespace kube-system
